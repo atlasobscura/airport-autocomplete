d2c7c7b97bb72237bd1274e1b4232a7ebad02363
Shiraj Ganguly <Shiraj.ganguly@atlasobscura.com>
2016-07-18 13:38:37 -0400

    Use Cors

--- web.js
+++ web.js
@@ -13,6 +13,7 @@ app.configure(function () {
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(cors());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
